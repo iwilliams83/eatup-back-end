8dc91f0ef5586da44f394583d67b56339481d13b
critsmet <36425627+critsmet@users.noreply.github.com>
2018-08-16 16:13:58 -0400

    Received search POST request from front end back end calls to Yelp API and returns results to front end

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -2,16 +2,10 @@ module Api
 	module V1
     class RestaurantsController < ApplicationController
 
-      def index
-        @param = search_params[:location]
+      def search
+        @param = params["location"]
         @response = RestClient.get 'https://api.yelp.com/v3/businesses/search', {:Authorization => 'Bearer fBK_qnU-H0uDkpc6mYiVG7VQtZbeHnqDvU9I0Xbfw7SxDBWxIRNzV-j-c4NaPBKj0eC97HYnlRZyBgchLuLusj6PCh488_TnRofoku7O6CPUKr_GTD1X218_Mo11W3Yx', :params => {:location => "#{@param}"}}
         render json: @response
-      end
-
-    private
-
-      def search_params
-        require(:restaurant).permit(:location)
       end
 
     end
