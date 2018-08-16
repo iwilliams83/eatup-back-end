9a916517513e4d02a80126ba1a7507bb2aae4b64
critsmet <36425627+critsmet@users.noreply.github.com>
2018-08-16 14:34:18 -0400

    Got GET request to Yelp API to return proper JSON data

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,14 +1,19 @@
-class RestaurantsController < ApplicationController
+module Api
+	module V1
+    class RestaurantsController < ApplicationController
 
-  def search
-    response = API CALL
-    render: JSON response
+      def index
+        @param = search_params[:location]
+        @response = RestClient.get 'https://api.yelp.com/v3/businesses/search', {:Authorization => 'Bearer fBK_qnU-H0uDkpc6mYiVG7VQtZbeHnqDvU9I0Xbfw7SxDBWxIRNzV-j-c4NaPBKj0eC97HYnlRZyBgchLuLusj6PCh488_TnRofoku7O6CPUKr_GTD1X218_Mo11W3Yx', :params => {:location => "#{@param}"}}
+        render json: @response
+      end
+
+    private
+
+      def search_params
+        require(:restaurant).permit(:location)
+      end
+
+    end
   end
-
-  private
-
-  def search_params
-    require(:restuarant).permit(:location)
-  end
-
 end
