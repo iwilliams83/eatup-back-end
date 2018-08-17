1da77067e08c0a43f99f3e761426eae5fc44d081
critsmet <36425627+critsmet@users.noreply.github.com>
2018-08-17 10:19:28 -0400

    Added User Controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,18 @@
+module Api
+	module V1
+    class UsersController < ApplicationController
+
+      def create
+         @user = User.new(name: params["name"], email: params["email"])
+         @user.save
+         render json: @user
+      end
+
+      def show
+        @user = User.find_by(email: params["email"])
+        render json: @user
+      end
+
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   namespace :api do
     namespace :v1 do
       post 'restaurants', :to => 'restaurants#search'
+      resources :users, only: [:create, :show, :index]
     end
   end
 
