39904ea21f082a9d3d0fc32c5c032c187050e28b
Indira Williams <indirajamurath@gmail.com>
2018-08-21 17:48:55 -0400

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2018_08_20_192236) do
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
